f87b74a987e6313e505e59901d63f9d3a3dfad16
Azure-Fluent <azfluent@microsoft.com>
2020-10-26 13:09:27 +0800

    Release azure-resourcemanager 2.0.0

--- src/main/java/com/azure/resourcemanager/keyvault/samples/ManageKeyVault.java
+++ src/main/java/com/azure/resourcemanager/keyvault/samples/ManageKeyVault.java
@@ -1,22 +1,20 @@
-/**
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for
- * license information.
- */
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
-package com.microsoft.azure.management.keyvault.samples;
+package com.azure.resourcemanager.keyvault.samples;
 
-import com.microsoft.azure.credentials.ApplicationTokenCredentials;
-import com.microsoft.azure.management.Azure;
-import com.microsoft.azure.management.keyvault.KeyPermissions;
-import com.microsoft.azure.management.keyvault.SecretPermissions;
-import com.microsoft.azure.management.keyvault.Vault;
-import com.microsoft.azure.management.resources.fluentcore.arm.Region;
-import com.microsoft.azure.management.resources.fluentcore.utils.SdkContext;
-import com.microsoft.azure.management.samples.Utils;
-import com.microsoft.rest.LogLevel;
-
-import java.io.File;
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.http.policy.HttpLogDetailLevel;
+import com.azure.core.management.AzureEnvironment;
+import com.azure.core.util.Configuration;
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.resourcemanager.AzureResourceManager;
+import com.azure.resourcemanager.keyvault.models.KeyPermissions;
+import com.azure.resourcemanager.keyvault.models.SecretPermissions;
+import com.azure.resourcemanager.keyvault.models.Vault;
+import com.azure.core.management.Region;
+import com.azure.core.management.profile.AzureProfile;
+import com.azure.resourcemanager.samples.Utils;
 
 /**
  * Azure Key Vault sample for managing key vaults -
@@ -33,14 +31,14 @@ public final class ManageKeyVault {
 
     /**
      * Main function which runs the actual sample.
-     * @param azure instance of the azure client
+     * @param azureResourceManager instance of the azure client
      * @param clientId client id
      * @return true if sample runs successfully
      */
-    public static boolean runSample(Azure azure, String clientId) {
-        final String vaultName1 = SdkContext.randomResourceName("vault1", 20);
-        final String vaultName2 = SdkContext.randomResourceName("vault2", 20);
-        final String rgName = SdkContext.randomResourceName("rgNEMV", 24);
+    public static boolean runSample(AzureResourceManager azureResourceManager, String clientId) {
+        final String vaultName1 = Utils.randomResourceName(azureResourceManager, "vault1", 20);
+        final String vaultName2 = Utils.randomResourceName(azureResourceManager, "vault2", 20);
+        final String rgName = Utils.randomResourceName(azureResourceManager, "rgNEMV", 24);
 
         try {
             //============================================================
@@ -48,7 +46,7 @@ public final class ManageKeyVault {
 
             System.out.println("Creating a key vault...");
 
-            Vault vault1 = azure.vaults().define(vaultName1)
+            Vault vault1 = azureResourceManager.vaults().define(vaultName1)
                     .withRegion(Region.US_WEST)
                     .withNewResourceGroup(rgName)
                     .withEmptyAccessPolicy()
@@ -83,6 +81,7 @@ public final class ManageKeyVault {
                     .withDeploymentEnabled()
                     .withTemplateDeploymentEnabled()
                     .updateAccessPolicy(vault1.accessPolicies().get(0).objectId())
+                    .allowCertificatePermissions()
                         .allowSecretAllPermissions()
                         .parent()
                     .apply();
@@ -95,7 +94,7 @@ public final class ManageKeyVault {
             //============================================================
             // Create another key vault
 
-            Vault vault2 = azure.vaults().define(vaultName2)
+            Vault vault2 = azureResourceManager.vaults().define(vaultName2)
                     .withRegion(Region.US_EAST)
                     .withExistingResourceGroup(rgName)
                     .defineAccessPolicy()
@@ -117,24 +116,22 @@ public final class ManageKeyVault {
 
             System.out.println("Listing key vaults...");
 
-            for (Vault vault : azure.vaults().listByResourceGroup(rgName)) {
+            for (Vault vault : azureResourceManager.vaults().listByResourceGroup(rgName)) {
                 Utils.print(vault);
             }
 
             //============================================================
             // Delete key vaults
             System.out.println("Deleting the key vaults");
-            azure.vaults().deleteById(vault1.id());
-            azure.vaults().deleteById(vault2.id());
+            azureResourceManager.vaults().deleteById(vault1.id());
+            azureResourceManager.vaults().deleteById(vault2.id());
             System.out.println("Deleted the key vaults");
 
             return true;
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
         } finally {
             try {
                 System.out.println("Deleting Resource Group: " + rgName);
-                azure.resourceGroups().deleteByName(rgName);
+                azureResourceManager.resourceGroups().beginDeleteByName(rgName);
                 System.out.println("Deleted Resource Group: " + rgName);
             } catch (NullPointerException npe) {
                 System.out.println("Did not create any resources in Azure. No clean up is necessary");
@@ -142,8 +139,6 @@ public final class ManageKeyVault {
                 g.printStackTrace();
             }
         }
-
-        return false;
     }
     /**
      * Main entry point.
@@ -155,17 +150,22 @@ public final class ManageKeyVault {
             //=============================================================
             // Authenticate
 
-            final File credFile = new File(System.getenv("AZURE_AUTH_LOCATION"));
+            final AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE);
+            final TokenCredential credential = new DefaultAzureCredentialBuilder()
+                .authorityHost(profile.getEnvironment().getActiveDirectoryEndpoint())
+                .build();
+            final Configuration configuration = Configuration.getGlobalConfiguration();
 
-            Azure azure = Azure.configure()
-                    .withLogLevel(LogLevel.BASIC)
-                    .authenticate(credFile)
-                    .withDefaultSubscription();
+            AzureResourceManager azureResourceManager = AzureResourceManager
+                .configure()
+                .withLogLevel(HttpLogDetailLevel.BASIC)
+                .authenticate(credential, profile)
+                .withDefaultSubscription();
 
             // Print selected subscription
-            System.out.println("Selected subscription: " + azure.subscriptionId());
+            System.out.println("Selected subscription: " + azureResourceManager.subscriptionId());
 
-            runSample(azure, ApplicationTokenCredentials.fromFile(credFile).clientId());
+            runSample(azureResourceManager, configuration.get(Configuration.PROPERTY_AZURE_CLIENT_ID));
         } catch (Exception e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
